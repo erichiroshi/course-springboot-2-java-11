33b9ac1e91134eb5463d367a2a67f86b357a53cb
erichiroshi <erichiroshi@gmail.com>
2020-12-15 12:12:05 -0400

    Category

---
+++ src/main/java/com/erichiroshi/course/entities/Category.java
@@ -0,0 +1,37 @@
+package com.erichiroshi.course.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+
+	public Category(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+}
