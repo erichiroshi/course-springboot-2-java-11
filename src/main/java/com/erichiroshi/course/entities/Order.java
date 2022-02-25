357d8ed3d2009601528c98b3428e8d5f6da2a486
erichiroshi <erichiroshi@gmail.com>
2022-02-25 13:43:36 -0400

    Payment, one-to-one association

--- src/main/java/com/erichiroshi/course/entities/Order.java
+++ src/main/java/com/erichiroshi/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.erichiroshi.course.entities.enums.OrderStatus;
@@ -45,6 +47,9 @@ public class Order implements Serializable{
 	
 	@OneToMany(mappedBy = "id.order")
 	@Getter Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	@Getter @Setter Payment payment;
 
 	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
---
+++ src/main/java/com/erichiroshi/course/entities/Payment.java
@@ -0,0 +1,38 @@
+package com.erichiroshi.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter @Setter private Long id;
+	@Getter @Setter private Instant moment;
+
+	@OneToOne
+	@MapsId
+	@Getter @Setter private Order order;
+	
+}
