513e791853367608a8bfb464bfcfed931ed32cda
erichiroshi <erichiroshi@gmail.com>
2022-02-25 13:48:01 -0400

    Subtotal & Total methods

--- src/main/java/com/erichiroshi/course/entities/OrderItem.java
+++ src/main/java/com/erichiroshi/course/entities/OrderItem.java
@@ -53,4 +53,7 @@ public class OrderItem implements Serializable {
 		id.setProduct(product);
 	}
 
+	public Double getSubTotal() {
+		return price * quantity;
+	}
 }
--- src/main/java/com/erichiroshi/course/entities/Payment.java
+++ src/main/java/com/erichiroshi/course/entities/Payment.java
@@ -11,6 +11,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -31,6 +33,7 @@ public class Payment implements Serializable {
 	@Getter @Setter private Long id;
 	@Getter @Setter private Instant moment;
 
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	@Getter @Setter private Order order;
